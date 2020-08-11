3287a64c9bf5df734ddbea55726ed6e1c2f7dc1d
Bozhen Zhao <zhaobozhen2025@gmail.com>
2020-08-11 13:02:24 +0800

    Update style.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,6 +25,7 @@ module.exports = {
       description: '应用架构查看'
     }
   },
+  theme: 'reco',
   themeConfig: {
     locales: {
       '/': {
@@ -41,7 +42,9 @@ module.exports = {
           '/guide/': getSidebar()
         },
         nav: getNavbar('/', '指南', '更新日志', '下载'),
-        lastUpdated: '最后更新'
+        lastUpdated: '最后更新',
+        mode: 'auto', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
+        modePicker: true // 默认 true，false 不显示模式调节按钮，true 则显示
       }
     },
     displayAllHeaders: true,
